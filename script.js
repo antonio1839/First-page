12f590cd8e119af00602d194e799b2d5941a2064
antonio1839 <antoniomh820@gmail.com>
2025-06-11 14:36:42 -0600

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let flores = document.querySelector(".flores-img");
-    setInterval(() => {
-        flores.style.transform = `rotate(${Math.random() * 4 - 2}deg) translateX(${Math.random() * 10 - 5}px)`;
-    }, 1000);
-});
